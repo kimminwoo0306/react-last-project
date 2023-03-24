9738c680b1c99e535a0898502ea26433241b6337
kimminwoo0306 <kimminou0306@naver.com>
2023-03-24 10:43:15 +0900

    React Project1

--- src/components/food/FoodFind.js
+++ src/components/food/FoodFind.js
@@ -23,8 +23,92 @@ function FoodFind(){
     const dataChange=(e)=>{
         setSs(e.target.value)
     }
+    const dataKeyDown=(e)=>{
+        setCurpage(1)
+        if(e.keyCode==13)
+        {
+            axios.get("http://localhost/jeju/food_find_react",{
+                params:{
+                    page:curpage,
+                    title:ss
+                }
+            }).then(response=>{
+                console.log(response.data)
+                setFdata(response.data)
+                setCurpage(response.data[0].curpage)
+                setTotalpage(response.data[0].totalpage)
+            })
+        }
+    }
     const findData=()=>{
-        // alert(ss)
+        setCurpage(1)
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:curpage,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setCurpage(response.data[0].curpage)
+            setTotalpage(response.data[0].totalpage)
+        })
+    }
+    /*
+    const prev=()=>{
+        setCurpage(curpage>1?curpage-1:curpage)
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:curpage,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setTotalpage(response.data[0].totalpage)
+        })
+    }
+    const next=()=>{
+        setCurpage(curpage<totalpage?curpage+1:curpage)
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page:curpage,
+                title:ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setTotalpage(response.data[0].totalpage)
+        })
+    }*/
+
+    const prev=()=>{
+        const prevPage = curpage>1?curpage-1:curpage;
+        setCurpage(prevPage);
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page: prevPage,
+                title: ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setTotalpage(response.data[0].totalpage)
+        })
+    }
+    const next=()=>{
+        const nextPage = curpage<totalpage ? curpage+1 : curpage;
+        setCurpage(nextPage);
+        axios.get("http://localhost/jeju/food_find_react",{
+            params:{
+                page: nextPage,
+                title: ss
+            }
+        }).then(response=>{
+            console.log(response.data)
+            setFdata(response.data)
+            setTotalpage(response.data[0].totalpage)
+        })
     }
     let html=fdata.map((food,index)=>
         <li className={index%4==0?'one_quarter first':'one_quarter'}>
@@ -40,7 +124,7 @@ function FoodFind(){
                     <div id="gallery">
                         <figure>
                             <header className="heading inline">
-                                <input type={"text"} size={"30"} className={"input-sm"} onChange={dataChange} value={ss}/>
+                                <input type={"text"} size={"30"} className={"input-sm"} onChange={dataChange} value={ss} onKeyDown={dataKeyDown}/>
                                 <input type={"button"} value={"검색"} className={"btn btn-sm btn-danger"} onClick={findData}/>
                             </header>
                             <ul className="nospace clear">
@@ -50,6 +134,12 @@ function FoodFind(){
                     </div>
 
                 </div>
+                <div className={"text-center"}>
+                    <button className={"btn btn-sm btn-primary"} onClick={prev}>이전</button>
+                    {curpage} page / {totalpage} pages
+                    <button className={"btn btn-sm btn-primary"} onClick={next}>다음</button>
+                </div>
+
                 <div className="clear"></div>
             </main>
         </div>
